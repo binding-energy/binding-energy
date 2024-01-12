02a3927f7ab0c45c7f8e4f429dfb30a95cda2525
binding-energy <hadizadeh1978@icloud.com>
2024-01-12 16:29:53 -0500

    animation change

--- src/Pages/CalculationScree.js
+++ src/Pages/CalculationScree.js
@@ -37,7 +37,7 @@ const CalulationScreen = () => {
             <div className="flex flex-row items-center justify-center h-screen bg-black " >
                 <div className="m-2 mb-3 self-center">
                     {/* <h1 className=" text-white text-3xl font-semibold ">Calculating<Typed className=" text-white text-3xl font-semibold " strings={["..."]} typeSpeed={120} backSpeed={140} loop /></h1> */}
-                    <h1 className=" text-white text-3xl font-semibold animate-ping">Calculating</h1>
+                    <h1 className=" text-white text-3xl font-semibold animate-bounce">Calculating</h1>
 
                 </div>
             </div>

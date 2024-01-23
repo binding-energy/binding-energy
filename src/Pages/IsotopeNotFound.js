61d726ca82b0f43400d75d9115dcb930e6ae4b9d
binding-energy <156280214+binding-energy@users.noreply.github.com>
2024-01-23 15:44:16 -0500

    Update IsotopeNotFound.js

--- src/Pages/IsotopeNotFound.js
+++ src/Pages/IsotopeNotFound.js
@@ -1,19 +1,28 @@
 import React from "react";
-
+import CustomButton from "../Components/CustomButton";
+import { useNavigate } from "react-router-dom";
 const IsotopeNotFound = () => {
+    const nav = useNavigate();
     return (
+
         <div>
             {/* <FontAwesomeIcon icon="fas fa-frown" /> */}
             {/* ICON */}
-            <div className="flex flex-row items-center justify-center h-screen bg-black " >
+            <div className="flex flex-col items-center justify-center h-screen bg-white " >
                 <div className="m-2 mb-3 self-center">
                     {/* <h1 className=" text-white text-3xl font-semibold ">Calculating<Typed className=" text-white text-3xl font-semibold " strings={["..."]} typeSpeed={120} backSpeed={140} loop /></h1> */}
-                    <h1 className=" text-white text-3xl font-semibold ">Sorry, but this Isotope is nonexistent!</h1>
+                    <h1 className=" text-black text-3xl font-semibold text-center">Sorry, but this Isotope is nonexistent,</h1>
+                    <h1 className=" text-black text-3xl font-semibold text-center">would you like you search again?</h1>
+
+
+                </div>
+                <div className="self-center">
+                    <CustomButton title={"GO BACK"} onClick={() => nav('/')} />
                 </div>
             </div>
             {/* BUTTON */}
             {/* <div>
-                <h1 className="text-center ">Go Back</h1>
+                <CustomButton title={"GO BACK"} />
             </div> */}
         </div>
     )

230f42d8929e1c4e92c22413795108b54d57c383
Hadizadeh <mrhadizadeh@gmail.com>
2024-02-17 00:09:24 -0500

    Feb17

--- src/Pages/CalculationScreen.js
+++ src/Pages/CalculationScreen.js
@@ -1,6 +1,6 @@
 
 // importing constant parameters
-import { mass_of_proton, mass_of_neutron, mass_of_electron, aV, aS, aC, aA, aP } from '../Functions/constants.js'
+import { mass_of_proton, mass_of_neutron, mass_of_electron, aV, aS, aC, aA, aP, conversion_amu_MeV } from '../Functions/constants.js'
 
 
 import React from "react";
@@ -70,7 +70,7 @@ const CalulationScreen = () => {
         )
     }
 
-    const atomic_mass = parseInt(data['atomic_mass']) / 1000000
+    const atomic_mass = parseFloat(data['atomic_mass']) / 1000000
     const atomic_number = parseInt(data['z'])
     const neutron_number = parseInt(data['n']);
     const element = elements(data['symbol']) + "-" + (atomic_number + neutron_number);
@@ -124,7 +124,7 @@ const CalulationScreen = () => {
                         <br />
                         Mass of Nuclide: m<sub>atom</sub> = {atomic_mass} amu
                         <br />
-                        Conversion Factor: 1 amu = 931.493614838934 MeV/c<sup>2</sup>
+                        Conversion Factor: 1 amu = {conversion_amu_MeV} MeV/c<sup>2</sup>
                         <br />
                         <hr class="hey-that’s-my-line" />
                         Δm = {mass_defect_func(atomic_mass, atomic_number, neutron_number).toFixed(8)} amu
